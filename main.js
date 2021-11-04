f523974388d85656ad6f83031e8313455a5c6369
evaughtbpf <evaughtbpf@gmail.com>
2021-11-03 21:42:48 -0400

    First commit of the swapi files

---
+++ main.js
@@ -0,0 +1,9 @@
+const residents = document.querySelector('button') 
+
+function buttonClicked () {
+    console.log("button clicked")
+}
+
+residents.addEventListener('click', buttonClicked)
+
+    
